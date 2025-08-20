283122c5764a038090e7d5c66abca49c41ded9db
AnaLu-Mantovani <peres.mantovani@escola.pr.gov.br>
2025-08-20 09:25:40 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,37 @@
 criaCartao(
-    'Programação',
-    'O que é Python',
-    'O Python é uma linguagem de programação.'
+    'Biologia',
+    'Qual a função da mitocôndria?',
+    'Sua função é a produção de energia na forma de ATP (adenosina trifosfato) através da respiração celular.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris.'
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília.'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa.'
+    'O que é um arquivo Style.css?',
+    'É um arquivo de texto que contém regras de estilo para controlar a aparência visual de um documento HTML ou XML.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em inglês?',
-    'Oi em inglês é Hi (RAI)'
+    'Como se diz Eu te amo em inglês?',
+    'Eu te amo em inglês é I love you.'
+
+)
+
+criaCartao(
+    'Química',
+    'Qual é o sexto elemento da tabela periódica?',
+    'O sexto elemento da tabela periódica é o Carbono.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo na informática?',
+    'É um conjunto finito de instruções bem definidas, usado para resolver um problema ou realizar uma tarefa específica.'
+
 )
